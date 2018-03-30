7fda665611acd7434d488421824dafe0fbf4e1ed
vkalia602 <vkalia602@gmail.com>
2018-03-30 00:18:54 +0000

    fixed syntax

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creating a file
-file { '/tmp/holberton':,
-path     => '/tmp/holberton',
-owner    => 'www-data',
-group    => 'www-data',
-mode     => '0744'
-content  => 'I love Puppet\n',
+file{'/tmp/holberton':
+	ensure   => 'file'
+	path     => '/tmp/holberton',
+	owner    => 'www-data',
+	group    => 'www-data',
+	mode     => '0744'
+	content  => 'I love Puppet\n',
 }
