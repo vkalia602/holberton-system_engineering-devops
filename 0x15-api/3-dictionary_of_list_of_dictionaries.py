b3d0aceaeb36239ec8d07d14c7776bdd35f23008
vkalia602 <vkalia602@gmail.com>
2018-05-01 03:34:32 +0000

    First commit

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Script that takes employee ID and returns information about that employee
+'''
+
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    total_tasks = []
+    all_employees = {}
+    list_userids = []
+    count = 0
+    employees = requests.get('{}users/'.format(url))
+    employees = employees.json()
+    for items in employees:
+        list_userids.append(items.get('id'))
+    for user in list_userids:
+        tasks = requests.get('{}users/{}/todos'
+                             .format(url, user))
+        tasks = tasks.json()
+        for items in tasks:
+            total_tasks.append(items)
+        all_employees[str(user)] = total_tasks
+
+    file_name = "{}.json".format('todo_all_employees')
+    with open(file_name, 'w') as f:
+        json.dump(all_employees, f)
