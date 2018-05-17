5eb96fadcc45ac1f1398e521afcff07c2ad9e989
vkalia602 <vkalia602@gmail.com>
2018-05-17 04:39:42 +0000

    Added comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# executes a command to change a typo in the wordpress configuration
 exec{ 'wp-settings.php':
       path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
       command  => 'sed -i -e "s/phpp/php/g" /var/www/html/wp-settings.php',
